101daa38e3910df657c36054425bbc88bd107441
vbiarunp <arunkumarp@visualbi.com>
2016-05-30 17:54:50 +0530

    Functions Added

--- www/js/index.js
+++ www/js/index.js
@@ -4,10 +4,9 @@ app.config(function($routeProvider){
     $routeProvider.when('/add', {
         templateUrl: 'views/add.html'
     })
-    .when('/search', {
+    .when('/search/:name', {
         templateUrl: 'views/search.html',
         controller: 'searchController',
-        paramExample: searchVal
     })
     .when('/home', {
         templateUrl: 'views/home.html'
@@ -19,30 +18,48 @@ app.config(function($routeProvider){
 
 app.controller("data", function($scope, $firebaseObject, $firebaseArray){
     var ref = new Firebase("https://getblood.firebaseio.com/");
-
     $scope.data = $firebaseObject(ref);
-
     var datas = $firebaseArray(ref);
     $scope.addData = function(){
+      var flag = 0;
+      var name = $scope.name;
+      var bloodType = $scope.blood;
+      var phoneNo = $scope.phonenumber;
+      var place = $scope.place;
+      angular.forEach($scope.data, function(value, key){
+          if(phoneNo == value.phone){
+            flag = 1;
+          }
+      });
+
+      if(flag == 0){
         datas.$add({
-            name: $scope.name,
-            blood: $scope.blood
+            name: name,
+            blood: bloodType,
+            phone: phoneNo,
+            place: place
         });
         $scope.name = '';
         $scope.blood = '';
+        $scope.phonenumber = '';
+        $scope.place = '';
+      }else{
+        alert("The phone number is already registered")
+      }
     }
 });
 
-app.controller("searchController", function($scope, $route, $firebaseObject, $firebaseArray){
-    var ref = new Firebase("https://getblood.firebaseio.com/");
-
-    $scope.getData = $firebaseObject(ref);
-
-    var paramValue = $route.current.$$route.paramExample;
-      console.log(paramValue); 
-    // angular.forEach($scope.getData, function(value, key){
-    //     console.log("f")
-    // });
-
+app.controller("searchController", function($scope, $routeParams, $firebaseObject, $firebaseArray){
+  var searchVal = $routeParams.name;
+  var ref = new Firebase("https://getblood.firebaseio.com/");
+  var data = $firebaseArray(ref);
+  $scope.filterData = [];
+  data.$loaded()
+    .then(function(){
+      angular.forEach(data, function(value, key){
+          if(searchVal == value.blood)
+            $scope.filterData.push(value)
+      });
+    });
 
 });
